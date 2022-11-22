0507692b7063341bebffa314715d7c0325424eaa
gkocaman <ggokhankocaman@gmail.com>
2022-11-22 17:26:53 +0300

    implicitlyWait kullanımı

--- src/lessonTwoSelenium/seleniumPractise.java
+++ src/lessonTwoSelenium/seleniumPractise.java
@@ -1,12 +1,15 @@
 package lessonTwoSelenium;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class seleniumPractise {
 
@@ -14,28 +17,37 @@ public class seleniumPractise {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "C://Drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		String[] itemsNeeded = {"Cucumber","Brocolli","Beetroot"};
-		int j=0;
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5000));
+
+		String[] itemsNeeded = { "Cucumber", "Brocolli", "Beetroot" };
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/");
-		Thread.sleep(3000);
+		addItems(driver, itemsNeeded);
+		driver.findElement(By.cssSelector("img[alt='Cart']")).click();
+		driver.findElement(By.cssSelector("div[class='cart-preview active'] button[type='button']")).click();
+		driver.findElement(By.xpath("//input[@class='promoCode']")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.xpath("//button[@class='promoBtn']")).click();
+		Assert.assertEquals(driver.findElement(By.xpath("//span[@class='promoInfo']")).getText(), "Code applied ..!") ;
+		driver.close();
+		
+	}
+
+	public static void addItems(WebDriver driver, String[] itemsNeeded) {
+		int j = 0;
 		List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
-		for(int i=0; i<products.size(); i++)
-		{
+		for (int i = 0; i < products.size(); i++) {
 			String[] name = products.get(i).getText().split("-");
-			String formattedName= name[0].trim();
+			String formattedName = name[0].trim();
 			List itemNeededList = Arrays.asList(itemsNeeded);
-			
-			if(itemNeededList.contains(formattedName)) {
+
+			if (itemNeededList.contains(formattedName)) {
 				j++;
 				driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
-				if(j==itemNeededList.size())   {
+				if (j == itemNeededList.size()) {
 					break;
 				}
-				
-				
+
 			}
 		}
-
 	}
 
 }
