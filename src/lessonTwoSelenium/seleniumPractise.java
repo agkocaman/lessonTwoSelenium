d3e53726d3dd03a6b1e2fc44d3831d9f310c1f1f
gkocaman <ggokhankocaman@gmail.com>
2022-11-22 08:21:27 +0300

    bir bug var burada ama örnek için kullanılır

--- src/lessonTwoSelenium/seleniumPractise.java
+++ src/lessonTwoSelenium/seleniumPractise.java
@@ -1,5 +1,6 @@
 package lessonTwoSelenium;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -9,16 +10,30 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class seleniumPractise {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "C://Drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		String[] itemsNeeded = {"Cucumber","Brocolli","Beetroot"};
+		int j=0;
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/");
 		List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
 		for(int i=0; i<products.size(); i++)
 		{
-			if(products.get(i).getText().contains("Cucumber")) {
+			String[] name = products.get(i).getText().split("-");
+			String formattedName= name[0].trim();
+			List itemNeededList = Arrays.asList(itemsNeeded);
+			Thread.sleep(1000);
+			if(itemNeededList.contains(formattedName)) {
+				j++;
 				driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
+				if(j==itemNeededList.size())   {
+					System.out.println("çıkış");
+					break;
+					
+				}
+				
+				
 			}
 		}
 
