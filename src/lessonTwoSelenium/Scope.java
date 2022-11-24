521858d586d51d22e35b5510d5c3ca6d4bf107ea
gkocaman <ggokhankocaman@gmail.com>
2022-11-24 23:09:38 +0300

    Iterator,  getWindowHandles

--- src/lessonTwoSelenium/Scope.java
+++ src/lessonTwoSelenium/Scope.java
@@ -1,21 +1,42 @@
 package lessonTwoSelenium;
 
+import java.util.Iterator;
+import java.util.Set;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Scope {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "C://Drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
-		System.out.println(driver.findElements(By.tagName("a")).size());
+		System.out.println("All"+driver.findElements(By.tagName("a")).size());
 		WebElement footer = driver.findElement(By.id("gf-BIG"));
-		System.out.println(footer.findElements(By.tagName("a")).size());
-
+		System.out.println("Footer"+footer.findElements(By.tagName("a")).size());
+		WebElement colouumDriver = driver.findElement(By.xpath("//tbody/tr/td[1]/ul[1]"));
+		System.out.println("coloum"+colouumDriver.findElements(By.tagName("a")).size());
+		for(int i=1; i<colouumDriver.findElements(By.tagName("a")).size(); i++) {
+			String clickOnLinkTab = Keys.chord(Keys.CONTROL,Keys.ENTER);
+			colouumDriver.findElements(By.tagName("a")).get(i).sendKeys(clickOnLinkTab);
+			Thread.sleep(5000L);
+		}
+			Set<String> abc = driver.getWindowHandles();
+			Iterator<String> it = abc.iterator();
+			int i = 0;
+			while (it.hasNext()) {
+				driver.switchTo().window(it.next());
+				System.out.println(driver.getTitle());
+				
+				System.out.println(i);
+				i++;
+			}
+			
 	}
 
 }
