fda6bb48aeda2a253eb0860ebb6e87056774e1ac
gkocaman <ggokhankocaman@gmail.com>
2022-11-24 22:18:36 +0300

    Scope

---
+++ src/lessonTwoSelenium/Scope.java
@@ -0,0 +1,21 @@
+package lessonTwoSelenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Scope {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C://Drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		System.out.println(driver.findElements(By.tagName("a")).size());
+		WebElement footer = driver.findElement(By.id("gf-BIG"));
+		System.out.println(footer.findElements(By.tagName("a")).size());
+
+	}
+
+}
