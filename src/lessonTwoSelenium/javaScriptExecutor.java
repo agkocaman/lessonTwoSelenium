a3adb0e068e603c5502fe59eb7cb429bf8f7d5c8
gkocaman <ggokhankocaman@gmail.com>
2022-11-26 00:42:24 +0300

    assert

--- src/lessonTwoSelenium/javaScriptExecutor.java
+++ src/lessonTwoSelenium/javaScriptExecutor.java
@@ -1,7 +1,12 @@
 package lessonTwoSelenium;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class javaScriptExecutor {
 
@@ -12,7 +17,15 @@ public class javaScriptExecutor {
 		JavascriptExecutor js = (JavascriptExecutor)driver;
 		js.executeScript("window.scroll(0,600)");
 		js.executeScript("document.querySelector(\".tableFixHead\").scroll(0,200)");
-
+		List<WebElement> values =  driver.findElements(By.xpath("//div[@class='tableFixHead']//td[4]"));
+		int sum = 0;
+		for(int i=0; i<values.size(); i++) {
+			
+			sum = sum +Integer.parseInt( values.get(i).getText());
+		}
+		System.out.println(sum);
+		int amount = Integer.parseInt(driver.findElement(By.cssSelector(".totalAmount")).getText().split(":")[1].trim()) ;
+		Assert.assertEquals(amount, sum);
 	}
 
 }
