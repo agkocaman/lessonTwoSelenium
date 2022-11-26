21eb5a183c9e309c87f1df8c555c0c812bbfb06c
gkocaman <ggokhankocaman@gmail.com>
2022-11-26 07:52:36 +0300

    screenshot pom.xml update

--- src/lessonTwoSelenium/miscelleanous.java
+++ src/lessonTwoSelenium/miscelleanous.java
@@ -1,21 +1,30 @@
 package lessonTwoSelenium;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.Cookie;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 
 
 public class miscelleanous {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws  IOException {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "C://Drivers/chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
-		driver.get("http://qaclickacademy.com/practice.php");
+		driver.get("http://www.google.com");
 		driver.manage().addCookie(new Cookie("key", "value"));
-		Thread.sleep(5000);
 		driver.manage().deleteAllCookies();
+		File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src,new File("C:\\Users\\GOKHAN\\homePageScreenshot.png"));  
+
 
 	}
 
