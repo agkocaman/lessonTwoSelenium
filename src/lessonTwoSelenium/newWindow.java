953cd25763907e497a13957314b293c6f658aa2d
gkocaman <ggokhankocaman@gmail.com>
2022-11-27 23:52:32 +0300

    screenShot details

--- src/lessonTwoSelenium/newWindow.java
+++ src/lessonTwoSelenium/newWindow.java
@@ -1,30 +1,37 @@
 package lessonTwoSelenium;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.Set;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class newWindow {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.setProperty("webdriver.chrome.driver", "C://Drivers/chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/angularpractice/");
 		driver.switchTo().newWindow(WindowType.TAB);
 		Set<String> handles = driver.getWindowHandles();
-		Iterator<String> it =handles.iterator();
+		Iterator<String> it = handles.iterator();
 		String par = it.next();
 		String chl = it.next();
 		driver.switchTo().window(chl);
 		driver.get("https://rahulshettyacademy.com");
 		String akademi = driver.findElement(By.xpath("(//h2/a[1])[1]")).getText();
 		driver.switchTo().window(par);
-		driver.findElement(By.xpath("(//input[@name='name'])[1]")).sendKeys(akademi);
-		
 
+		WebElement aka = driver.findElement(By.xpath("(//input[@name='name'])[1]"));
+		aka.sendKeys(akademi);
+		File file = aka.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(file, new File("deneme.png"));
 	}
 
 }
